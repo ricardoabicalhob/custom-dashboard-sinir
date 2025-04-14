0577defc946b563defa95ae58f51223dac0c8a55
ricardoabicalhob <rbicalho2014@gmail.com>
2025-04-14 11:05:45 -0300

    fazer instalação de date-fns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,9 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const extendedConfig = compat.extends("next/core-web-vitals", "next/typescript");
+// const extendedConfig = compat.extends("next/core-web-vitals", "next/typescript");
 
-console.log(JSON.stringify(extendedConfig, null, 2));
+// console.log(JSON.stringify(extendedConfig, null, 2));
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript")
