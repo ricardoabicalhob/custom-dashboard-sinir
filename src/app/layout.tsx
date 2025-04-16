893f2486096f306faef7dfbbcdf2cf39db54fa09
ricardoabicalhob <rbicalho2014@gmail.com>
2025-04-15 23:10:09 -0300

    corrigir erro de metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
-'use client'
-
 import type { Metadata } from "next";
 import "./globals.css";
-import { QueryClientProvider } from "react-query";
-import { queryClient } from "@/services/queryClient";
 
-// export const metadata: Metadata = {
-//   title: "Gestão de Resíduos - SINIR",
-//   description: "Ferramenta de análise para gestão de resíduos integrada ao SINIR",
-// };
+export const metadata: Metadata = {
+  title: "Gestão de Resíduos - SINIR",
+  description: "Ferramenta de análise para gestão de resíduos integrada ao SINIR",
+};
 
 export default function RootLayout({
   children,
@@ -16,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang={`pt-BR`}>
-        <body>{children}</body>
-      </html>
-    </QueryClientProvider>
+    <html lang={`pt-BR`}>
+      <body>{children}</body>
+    </html>
   );
 }
